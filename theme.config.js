84fc57ca338447d2939c10f675c3ddd27f23404a
José Avila <aviladiazjose09>
2022-01-18 01:58:43 -0500

    se agrega seccion linux

--- theme.config.js
+++ theme.config.js
@@ -4,9 +4,9 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">José Avila</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        My second brain
       </span>
     </>
   ),
