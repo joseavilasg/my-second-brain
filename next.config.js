0a19160b04dd8a5fc9b5182084f54e3cad74a0b7
joseavilasg <joseavilasg.dev@gmail.com>
2024-04-09 21:57:23 -0500

    Update to new version

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
-  unstable_staticImage: true,
-})
-module.exports = withNextra()
-module.exports.images = {
-  ...module.exports,
-  domains: ['spblog.net'],
+/**
+ * @type {import('next').NextConfig}
+*/
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  staticImage: true,
+});
+
+const nextConfig = {
+  images: {
+    domains: ['spblog.net'],
+  }
 }
+
+module.exports = withNextra(nextConfig)
---
+++ theme.config.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+	logo: (
+		<span>
+			<b>José Avila SG</b> - My Secod Brain
+		</span>
+	),
+	project: {
+		link: "https://github.com/joseavilasg/my-second-brain",
+	},
+	chat: {
+		link: "https://discord.com",
+	},
+	docsRepositoryBase: "https://github.com/joseavilasg/my-second-brain",
+	footer: {
+		text: "José Avila SG. Original template from Nextra.",
+	},
+};
+
+export default config;
